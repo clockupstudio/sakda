5b190a61cd7f74268dee8fd3fbb0e594a206e4d5
Prayoch Rujira <jay@odd-e.co.th>
2017-01-21 18:39:45 +0700

    Add Energet script

---
+++ Assets/Scripts/Energy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Energy : MonoBehaviour
+{
+
+    private float time = 100f;
+    public Text energyText;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime * 2f;
+		energyText.text = String.Format("Energy: {0:0}", time);
+    }
+}
