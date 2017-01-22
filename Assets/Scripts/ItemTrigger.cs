6fb0454b3b6c391f8c25a0be345e45dd7186c7eb
Prayoch Rujira <jay@odd-e.co.th>
2017-01-22 14:45:19 +0700

    Add collect sound

--- Assets/Scripts/ItemTrigger.cs
+++ Assets/Scripts/ItemTrigger.cs
@@ -2,11 +2,13 @@
 
 public class ItemTrigger : MonoBehaviour {
 
+	public AudioClip collectSound;
 	void OnTriggerEnter2D(Collider2D other){
 		if (other.tag == "Sakda")
         {
 			gameObject.SetActive(false);
 			Energy.instance.Increase(5);
+			SoundManager.instance.PlaySingle(collectSound);
 		}
 	}
 }
